3619d7a959b4a9aed8a902772043a4905433ed1f
Namkhingnrs <noeynrrsh@gmail.com>
2026-03-23 17:23:45 +0700

    feat(boards): generate boards resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
